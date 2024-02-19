7dbb56db59905cd179f95767ac9161ae047af7c6
Kun Zhao <zhaokun.km@gmail.com>
2024-02-19 22:22:11 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pip install flask
-pip install flask_sock
+pip install flask-sock
 pip install websocket-client 
 
 /workspace/ComfyUI/venv/bin/flask --app 'gligen_gui:create_app(8188)' run --port 5000 --host 0.0.0.0
